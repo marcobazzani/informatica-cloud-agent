54f65cf304971d3ff4bbfc9cb275accf32c85ac5
Marco <github@marcobazzani.name>
2016-07-10 18:05:28 +0200

    input validation

--- docker_agent_start.sh
+++ docker_agent_start.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+if [ ! $# -eq 2 ] ; then
+    echo "No username password provided"
+    echo "please provide username and password"
+    exit 10
+fi
+
 set -e
 cd /root/infaagent
 ./agent_start.sh  &
